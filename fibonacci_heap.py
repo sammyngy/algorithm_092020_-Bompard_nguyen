2acb06d9589fad274533f0e861e898e4becaf4d5
Sammy <tehstormdzn@icloud.com>
2020-09-30 20:25:20 +0200

    ok

--- fibonacci_heap.py
+++ fibonacci_heap.py
@@ -45,27 +45,67 @@ class FibonacciHeap(Heap):
     L'implémentation est décrite en anglais : https://en.wikipedia.org/wiki/Fibonacci_heap#Implementation_of_operations
     et en français : https://fr.wikipedia.org/wiki/Tas_de_Fibonacci#Implémentation_des_opérations
     """
+    def __init__(self):
+        self.trees = []
+        self.branch = []
+   
+    
 
     def insert(self, value: int) -> None:
         """
         Ajoute une valeur dans l'arbre
         """
-        pass
+        self.trees.append(value)
+
+     
 
     def find_min(self) -> int:
         """
         Retourne la valeur minimum dans l'arbre
         """
-        pass
+        min_value = None 
+        for value in self.trees:
+            if not min_value:
+                min_value = value
+            elif value < min_value:
+                min_value = value
+        print(min_value)
+        return min_value
+
+       
 
     def delete_min(self) -> int:
         """
         Supprime et retourne la valeur minimum dans l'arbre
         """
+        minValue = self.find_min()
+        if minValue is not None:
+            self.trees.remove(minValue)
         pass
 
     def merge(self, fibonnaci_heap: Heap) -> None:
         """
         Fusionne deux arbres
         """
+        new = self.trees + heap
+        print(new)
         pass
+
+
+heap = FibonacciHeap()
+heap.insert(5)
+heap.insert(1)
+heap.insert(10)
+heap.insert(0)
+heap.insert(42)
+heap.insert(15)
+heap.insert(7)
+heap.insert(19)
+heap.insert(20)
+heap.insert(2)
+heap.insert(84)
+heap.insert(50)
+heap.find_min()
+heap.delete_min()
+
+heap.merge(heap)
